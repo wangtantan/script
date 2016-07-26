71388d9fc7a65bd1d783f20614047593cd75fa96
tantan <wangtantan09@163.com>
2016-07-26 19:57:57 +0800

    fix

--- sync_redis_to_bak.sh
+++ sync_redis_to_bak.sh
@@ -1,20 +1,45 @@
-#!/bin/bash -xv
+#!/bin/bash
 
 formatDate=$(date +%F)
 host=mongo
 toRoot=/data/redis
 
-cd $toRoot && /usr/local/bin/redis-cli -h mongo --rdb dump.rdb.$(date +%F) \
-  || echo "dump redis dataBase failed: 2" && exit 2
+echo "start sync redis from $host to localhost"
+systemctl is-active redis
+if [ $? != 0 ]; then
+  echo "redis is stopped: 1"
+  exit 1
+fi
+echo "redis is active"
+cd $toRoot && /usr/local/bin/redis-cli -h mongo --rdb dump.rdb.$(date +%F)
+if [ $? != 0 ]; then
+  echo "dump redis dataBase failed: 2"
+  exit 2
+fi
 echo "dump redis dataBase success: dump.rdb.$(formatDate)"
 
-systemctl stop redis || echo "redis can't be stopped fail: 3" && exit 3
+systemctl stop redis
+
+if systemctl is-active redis; then
+  echo "redis can't be stopped fail: 3"
+  exit 3
+fi
 echo "redis is stopped with success"
 
-[ -s "dump.rdb" ] && mv dump.rdb dump.rdb.bak
-cp dump.rdb.$(formatDate) dump.rdb || echo "copy rdb with fail: 4" && exit 4
+[ -s "$toRoot/dump.rdb" ] && cp $toRoot/dump.rdb $toRoot/dump.rdb.bak
+cp $toRoot/dump.rdb.$(formatDate) $toRoot/dump.rdb
+if [ $? != 0 ]; then
+  echo "copy rdb with fail: 4"
+  exit 4
+fi
 echo "copy dump.rdb with success"
 
-systemctl start redis && systemctl is-active redis || echo "restart fail: 5" && exit 5
+systemctl start redis
+if [ $? != 0 ]; then
+  echo "restart with fail: 5"
+  exit 5
+fi
 
-echo "sync rdb and restart with success" && exit 0
+echo "sync rdb and restart with success"
+exit 0
+
