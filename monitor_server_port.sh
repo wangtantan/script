da65db728db58b95ba448f9f1bb0f8b8d7034959
lutaoact <lutaoact@hotmail.com>
2016-08-01 18:39:08 +0800

    add notice script

--- monitor_server_port.sh
+++ monitor_server_port.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 #lastRestart=10
-lastRestart=$(pm2 show app | grep restarts | grep -v unstable | awk '{print $4}')
+lastRestart=$(pm2 show entering | grep restarts | grep -v unstable | awk '{print $4}')
 
 while true; do
   if nc -z localhost 9003; then
     echo "9003端口正常"
   else
     echo "9003端口异常"
-    #云信通知
+    #node ~/node-server/scripts/pushNetEaseMessage.js 2001
   fi
 
   sleep 1
 #  newRestart=15
-  newRestart=$(pm2 show app | grep restarts | grep -v unstable | awk '{print $4}')
+  newRestart=$(pm2 show entering | grep restarts | grep -v unstable | awk '{print $4}')
   count=$[$newRestart-$lastRestart]
   if [ $count -gt 3 ]; then
-    echo "重启过多"
-    #云信通知
+    echo "重启过多: ${count}"
+    #node ~/node-server/scripts/pushNetEaseMessage.js 2001
   else
-    echo "重启正常"
+    echo "重启正常: ${newRestart}"
   fi
   lastRestart=$newRestart
 done
