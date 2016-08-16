5eeb7149f2eba1eaab6345ef6ca27c9a669d711e
lutaoact <lutaoact@hotmail.com>
2016-08-16 17:59:36 +0800

    add build_csv_by_multi_cpu.sh

--- build_csv_by_multi_cpu.sh
+++ build_csv_by_multi_cpu.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 cd ~/node-server2
-timeTable=(20160423 20160501 20160511 20160521 20160601 20160611 20160621 20160701 20160711)
+timeTable=(20160423 20160501 20160511 20160521 20160601 20160611 20160621 \
+           20160701 20160711 20160721 20160801 20160811 20160821)
 
-for ((i=0; i<8; i++))
+for ((i=0; i<12; i++))
 do
-  ('scripts/buildStocksAvgEffectCVS.js'  ${timeTable[$i]} ${timeTable[$[$i+1]]})
+#echo ${timeTable[$i]} ${timeTable[$[$i+1]]}
+  echo "start ${timeTable[$i]}"
+  node scripts/buildStocksAvgEffectCVS.js  \
+    ${timeTable[$i]} ${timeTable[$[$i+1]]} \
+    >>/dev/null 2>&1
+  echo "finash ${timeTable[$i]}"
 done
