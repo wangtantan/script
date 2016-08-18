c65462afd644d3a0e018f0719203786bbcf1f5b4
lutaoact <lutaoact@hotmail.com>
2016-08-18 11:04:44 +0800

    fix

--- build_csv_by_multi_cpu.sh
+++ build_csv_by_multi_cpu.sh
@@ -7,9 +7,9 @@ timeTable=(20160423 20160501 20160511 20160521 20160601 20160611 20160621 \
 for ((i=0; i<12; i++))
 do
 #echo ${timeTable[$i]} ${timeTable[$[$i+1]]}
-  echo "start ${timeTable[$i]}"
+  echo "start $i"
   node scripts/buildStocksAvgEffectCVS.js  \
     ${timeTable[$i]} ${timeTable[$[$i+1]]} \
     >>/dev/null 2>&1
-  echo "finash ${timeTable[$i]}"
+  echo "finash ${timeTable[$i]} to ${timeTable[$[$i+1]]} "
 done
